9a51e9bc663858992d11efdb96a8ec05b95305b2
Dan Steingart <dan.steingart@columbia.edu>
2023-03-08 14:41:34 -0500

    Update server.js
    added timeout to make MP Cadet Work

--- server.js
+++ server.js
@@ -265,7 +265,7 @@ app.get('/lastread/',function(req,res){
 //read buffer
 app.get('/read/', function(req, res){ res.send(buf) });
 
-
+if (process.env.CADET=="true") setTimeout(()=>{sender=true},10000);
 setInterval(()=>{serialPort.write("M114\r\n")},250)
 
 //weak interface
